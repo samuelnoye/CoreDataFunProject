d4496389799b53183ec68d608ecad4703b95208e
Samuel Noye <samuel.noye@amalitech.org>
2022-09-13 09:32:54 +0000

    update

---
+++ CoreDataFunProject/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  CoreDataFunProject
+//
+//  Created by Samuel Noye on 07/09/2022.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
