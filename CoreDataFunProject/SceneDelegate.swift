1804a320dddaf8e0eba8c6d7e609bfedd32023af
Samuel Noye <samuel.noye@amalitech.org>
2022-09-13 09:33:34 +0000

    Update SceneDelegate.swift

--- CoreDataFunProject/SceneDelegate.swift
+++ CoreDataFunProject/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.makeKeyAndVisible()
-        let controller = ViewController()
+      //  let controller = ViewController()
         window?.rootViewController = UINavigationController(rootViewController: controller)
     }
 
